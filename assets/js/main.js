5ccf09f78f1ac6507a3b9d79e5aeed5391306daa
10xMike <michelegec@gmail.com>
2021-05-10 00:16:45 +0200

    Creato oggetto, ciclo for-in, array di studenti

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,37 @@
 // Creare un oggetto che descriva uno studente, con le seguenti proprietà: nome, cognome, età
+var studente = {
+    nome: "Shauna",
+    cognome: "Vayne",
+    age: 24,
+}
 
 // Stampare a schermo attraverso un ciclo for-in tutte le proprietà dell'oggetto
+for (var key in studente) {
+    console.log(studente);
+    document.getElementById('studente').innerHTML += [key] + ": " + studente[key] + "<br>";
+}
 // Creare un array di oggetti di studenti
+var studenti = [
+    {
+        nome: "Gregory",
+        cognome: "House",
+        age: 61,
+    },
+    {
+        nome: "Lisa",
+        cognome: "Cuddy",
+        age: 54,
+    },
+    {
+        nome: "James",
+        cognome: "Wilson",
+        age: 52,
+    },
+    {
+        nome: "Allison",
+        cognome: "Cameron",
+        age: 42,
+    }
+]
 // Ciclare su tutti gli studenti e stampare per ognuno di essi, nome e cognome
 // Dare la possibilità all’utente, attraverso 3 prompt(), di aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età
